9e615bdc2fd5ea27665b4a95494ae1cbfe25c69e
padmasowgandhika77 <padmasowgandhika77@gmail.com>
2018-08-06 21:42:51 +0530

    Update cart.py

--- booksapp/views/cart.py
+++ booksapp/views/cart.py
@@ -16,15 +16,12 @@ class CartListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(CartListView, self).get_context_data(**kwargs)
-
         id1 =  self.request.user.id
-        order_object = Order.objects.filter(owner_id = self.request.user.id)
-
-        cart_items =[ i.items.all() for i in order_object]
-        cart_products = [j for i in cart_items for j in i]
-
-        for j in order_object:
-            total = Order.get_cart_total(j)
+        total=0
+        user_profile = get_object_or_404(Profile, user_pf_id  = self.request.user.id)
+        order_object,status = Order.objects.get_or_create(owner=user_profile)
+        cart_products = order_object.items.all()
+        total = Order.get_cart_total(order_object)
 
         context.update({
             'cart':cart_products,
@@ -34,20 +31,64 @@ class CartListView(ListView):
 
         return context
     pass
+# class CartListView(ListView):
+#     model = Order
+#     context_object_name = "cart"
+#     template_name = "booksapp/html_template/cart.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super(CartListView, self).get_context_data(**kwargs)
+
+#         id1 =  self.request.user.id
+#         order_object = Order.objects.filter(owner_id = (self.request.user.id))
+
+#         cart_items =[ i.items.all() for i in order_object]
+#         cart_products = [j for i in cart_items for j in i]
+
+#         if order_object:
+#              for j in order_object:
+#                 total = Order.get_cart_total(j)
+#         else:
+#             total = 0
+
+
+#         context.update({
+#             'cart':cart_products,
+#             'total':total,
+#             'user_permissions': self.request.user.get_all_permissions
+#         })
+
+#         return context
+#     pass
 
 def add_to_cart(request,**kwargs):
     user_profile = get_object_or_404(Profile, user_pf_id = request.user.id)
     product = Products.objects.filter(id = kwargs.get('pk',"")).first()
-
     order_item,status = OrderItem.objects.get_or_create(product = product)
+
+    if(status):
+        pass
+    else:
+        pass
+
     user_order,status = Order.objects.get_or_create(owner=user_profile)
     user_order.items.add(order_item)
-    if status:
-        #user_order.ref_code = generate_order_id()
-        user_order.save()
-
+    user_order.save()
     messages = info(request,"Item Added To Cart")
-    return redirect(reverse("biblioapp:show_products"))
+    return redirect(reverse('biblioapp:view_cart'))
+# def add_to_cart(request,**kwargs):
+#     user_profile = get_object_or_404(Profile, user_pf_id = (request.user.id))
+#     product = Products.objects.filter(id = kwargs.get('pk',"")).first()
+
+#     order_item,status = OrderItem.objects.get_or_create(product = product)
+#     user_order,status = Order.objects.get_or_create(owner=user_profile)
+#     user_order.items.add(order_item)
+#     if status:
+#         #user_order.ref_code = generate_order_id()
+#         user_order.save()
+
+#     messages = info(request,"Item Added To Cart")
+#     return redirect(reverse("biblioapp:view_cart"))
 
 class OrderItemDeleteView(DeleteView):
     model=OrderItem
@@ -67,3 +108,27 @@ def generate_order_id():
     date_str = date.today().strftime('%Y%m%d')[2:] + str(datetime.now().second)
     rand_str = "".join([random.choice(string.digits) for count in range(3)])
     return date_str + rand_str
+
+def increment_units(request,**kwargs):
+    prod = get_object_or_404(Products, pk=kwargs['pk'])
+    user_profile = get_object_or_404(Profile, user_pf_id=request.user.id)
+    objs = Order.objects.get(owner=user_profile)
+
+    for i in objs.items.all():
+        if(i.product.id == prod.id):
+            c = i
+    c.qty = c.qty + 1
+    c.save()
+    return redirect('biblioapp:view_cart')
+
+def decrement_units(request,**kwargs):
+    prod = get_object_or_404(Products, pk=kwargs['pk'])
+    user_profile = get_object_or_404(Profile, user_pf_id=request.user.id)
+    objs = Order.objects.get(owner=user_profile)
+
+    for i in objs.items.all():
+        if(i.product.id == prod.id):
+            c = i
+    c.qty = c.qty - 1
+    c.save()
+    return redirect('biblioapp:view_cart')
